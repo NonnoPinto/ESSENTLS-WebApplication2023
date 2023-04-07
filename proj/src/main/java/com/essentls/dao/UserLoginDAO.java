64e0e320a65806747c40286065f93a23cc9fe724
andrea-campagnol <andrea.campagnol@gmail.com>
2023-04-07 21:43:30 +0200

    #39 Added UserPaymentsListDAO class

--- proj/src/main/java/com/essentls/dao/UserLoginDAO.java
+++ proj/src/main/java/com/essentls/dao/UserLoginDAO.java
@@ -1,5 +1,7 @@
 package com.essentls.dao;
 
+import com.essentls.resource.User;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
---
+++ proj/src/main/java/com/essentls/dao/UserPaymentsListDAO.java
@@ -0,0 +1,86 @@
+package com.essentls.dao;
+
+import com.essentls.resource.User;
+import com.essentls.resource.Payment;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Gather payments related to a user.
+ *
+ * @author Andrea Campagnol (andrea.campagnol.1@studenti.unipd.it)
+ * @version 1.00
+ * @since 1.00
+ */
+public final class UserPaymentsListDAO extends AbstractDAO<List<Payment>> {
+
+    /**
+     * The SQL statement to be executed
+     */
+    private static final String STATEMENT = "SELECT * FROM Payment WHERE userID = ?";
+
+    /**
+     * The user that made the payments
+     */
+    private final User user;
+
+    /**
+     * Creates a new object for gather payments by user.
+     *
+     * @param con    the connection to the database.
+     * @param user   the user that made the payments.
+     */
+    public UserPaymentsListDAO(final Connection con, final User user) {
+        super(con);
+        this.user = user;
+    }
+
+    @Override
+    public final void doAccess() throws SQLException {
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        final List<Payment> payments = new ArrayList<Payment>();
+
+        try {
+            stmt = con.prepareStatement(STATEMENT);
+            stmt.setInt(1, this.user.getID());
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                payments.add(
+                        new Payment(
+                                rs.getInt("ID"),
+                                rs.getInt("UserID"),
+                                rs.getInt("EventID"),
+                                rs.getString("Method"),
+                                rs.getFloat("Amount"),
+                                rs.getDate("Date"),
+                                rs.getString("Notes")
+                        )
+                );
+            }
+
+            LOGGER.info("Pyament(s) related to user %s successfully listed.", this.user.getEmail());
+
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+
+            if (stmt != null) {
+                stmt.close();
+            }
+
+        }
+
+        this.outputParam = payments;
+        con.close();
+    }
+}
