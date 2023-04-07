7f2772f261de1b5aca902347184ce66584aedc3c
andrea-campagnol <andrea.campagnol@gmail.com>
2023-04-07 18:58:09 +0200

    #27 Added UserLoginDAO class

---
+++ proj/src/main/java/com/essentls/dao/UserLoginDAO.java
@@ -0,0 +1,118 @@
+package com.essentls.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Authenticates the user in the database
+ */
+public final class UserLoginDAO extends AbstractDAO<User> {
+//AbstractDAO<User> return the user logged in
+
+    /**
+	 * The SQL statement to be executed
+	 */
+    private static final String STATEMENT_LOGIN = "SELECT * FROM User WHERE email=? AND password=md5(?);"; //md5() not secure?
+
+	/**
+	 * The email of the user to be authenticated
+	 */
+	private final String email;
+
+	/**
+	 * The password of the user to be authenticated
+	 */
+	private final String password;
+
+    /**
+	 * Creates a new object for loggin in a user by its email and password.
+	 *
+	 * @param con      the connection to the database.
+	 * @param email    the email of the user to be authenticated.
+	 * @param password the password of the user to be authenticated.
+	 */
+    public UserLoginDAO(final Connection con, final String email, final String password) {
+        super(con);
+
+        if (email == null || email.isBlank()) {
+			LOGGER.error("The email cannot be null or empty.");
+			throw new NullPointerException("The email cannot be null or empty.");
+		}
+
+		this.email = email;
+
+		if (password == null || password.isBlank()) {
+			LOGGER.error("The password cannot be null or empty.");
+			throw new NullPointerException("The password cannot be null or empty.");
+		}
+
+		this.password = password;
+    }
+
+    @Override
+    public void doAccess() throws SQLException {
+
+        PreparedStatement stmnt = null;
+        ResultSet rs = null;  // the results of the search
+        User user = null; //user returned to outputParam if logged in 
+
+        try {
+            stmnt = con.prepareStatement(STATEMENT_LOGIN);
+            stmnt.setString(1, this.email);
+            stmnt.setString(2, this.password);
+            rs = stmnt.executeQuery();
+
+            if(rs.next()){
+
+                user = new User(
+                            rs.getInt("Id"),
+                            rs.getString("Email"),
+                            rs.getString("Password"),
+                            rs.getInt("CardID"),
+                            rs.getInt("Tier"),
+                            rs.getDate("RegistrationDate"),
+                            rs.getString("Name"),
+                            rs.getString("Surname"),
+                            rs.getstring("Sex"),
+                            rs.getDate("DOB"),
+                            rs.getString("Nationality"),
+                            rs.getString("HomeCountryAddress"),
+                            rs.getString("HomeCountryUniversity"),
+                            rs.getString("PeriodOfStay"),
+                            rs.getString("PhoneNumber"),
+                            rs.getString("PadovaAddress"),
+                            rs.getString("DocumentType"),
+                            rs.getstring("DocumentNumber"),
+                            rs.getString("DocumentFile"),
+                            rs.getString("DietType"),
+                            rs.getString("Allergies"),
+                            rs.getString("EmailHash"),
+                            rs.getString("EmailConfirmed"),
+                ); 
+
+                LOGGER.info("User logged in {}.", user.getEmail());
+
+            }else{
+
+                LOGGER.error("error logging in the user {}", user.getEmail());
+
+            }
+
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+
+            if (stmnt != null) {
+                stmnt.close();
+            }
+
+            this.outputParam = user; 
+            con.close();
+        }
+
+    }
+    
+}
