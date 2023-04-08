2cdf86b590f96bcc5439b764aadf2df6d8a16cdc
Vittorio Cardillo <vitto.cardillo.21@gmail.com>
2023-04-08 17:14:08 +0200

    #37 Added JoinedEventsListDAO

---
+++ proj/src/main/java/com/essentls/dao/UserJoinsEventDAO.java
@@ -0,0 +1,48 @@
+package com.essentls.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.essentls.resource.Participant;
+
+public class UserJoinsEventDAO extends AbstractDAO {
+    private static final String STATEMENT_JOIN_EVENT = "INSERT INTO data.participant (userId, eventId, role, date, attributeValues) VALUES (?, ?, ?, ?, ?)";
+
+    private final Participant participant;
+
+    public UserJoinsEventDAO(Connection con, Participant participant) {
+        super(con);
+
+        if (participant == null) {
+            LOGGER.error("The participant cannot be null.");
+            throw new NullPointerException("The participant cannot be null.");
+        }
+
+        this.participant = participant;
+    }
+
+    @Override
+    protected void doAccess() throws Exception {
+
+        PreparedStatement pstmt = null;
+
+        try {
+            pstmt = con.prepareStatement(STATEMENT_JOIN_EVENT);
+            pstmt.setLong(1, participant.getUserId());
+            pstmt.setLong(2, participant.getEventId());
+            pstmt.setString(3, participant.getRole());
+            pstmt.setDate(4, java.sql.Date.valueOf(participant.getDate()));
+
+            pstmt.execute();
+
+            LOGGER.info("User %l successfully join event %l.", participant.getUserId(), participant.getEventId());
+
+        } finally {
+            if (pstmt != null)
+                pstmt.close();
+        }
+        
+    }
+
+
+}
