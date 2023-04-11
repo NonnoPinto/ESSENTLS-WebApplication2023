2c6d951a810f89fe21e6b939ad95703cde82257e
Laura <laurapallante1999@gmail.com>
2023-04-11 19:13:43 +0200

    Added comments to UserPaymentSubmitDAO

--- proj/src/main/java/com/essentls/dao/UserPaymentSubmitDAO.java
+++ proj/src/main/java/com/essentls/dao/UserPaymentSubmitDAO.java
@@ -6,13 +6,33 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-
+/**
+ * Add a user payment to the payment's table
+ *
+ * @author Laura Pallante (laura.pallante@studenti.unipd.it
+ * @version 1.00
+ * @since 1.00
+ */
 
 public class UserPaymentSubmitDAO extends AbstractDAO<Payment> {
+
+    /**
+     * The SQL statement to be executed
+     */
     private static final String STATEMENT = "INSERT INTO Payments (id, userId, eventId, method, amount, date, notes) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
+    /**
+     * The payment that must be added
+     */
     private final Payment payment;
 
+
+    /**
+     * Creates a new object for the updating of the tier of a user
+     *
+     * @param con    the connection to the database.
+     * @param payment  the payment that must be added
+     */
     public UserPaymentSubmitDAO(Connection con, final Payment payment) {
         super(con);
         this.payment = payment;
@@ -33,7 +53,7 @@ public class UserPaymentSubmitDAO extends AbstractDAO<Payment> {
             stmt.setDate(6, payment.getDate());
             stmt.setString(7, payment.getNotes());
 
-            stmt.executeUpdate();
+            stmt.executeUpdate();   //add to payment's table
 
             LOGGER.info("Payment %l successfully added to the payment's list.", payment.getId());
         } finally {
