4f3d4c1782332c607c9a2d97051ee972c103f208
Francesco Marcato <49764729+marcatofrancesco@users.noreply.github.com>
2023-04-16 22:57:52 +0200

    #62 eventcreation.jsp and dedicated servlet

---
+++ proj/src/main/java/com/essentls/servlet/CreateEventServlet.java
@@ -0,0 +1,112 @@
+package com.essentls.servlet;
+
+import com.essentls.dao.*;
+import com.essentls.resource.*;
+import java.sql.*;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public final class CreateEventServlet extends AbstractDatabaseServlet {
+
+    /**
+     * Creates a new event into the database.
+    *
+    * @param req the HTTP request from the client.
+    * @param res the HTTP response from the server.
+    *
+    * @throws IOException if any error occurs in the client/server communication.
+    */
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+        LogContext.setIPAddress(req.getRemoteAddr());
+        LogContext.setAction("CREATE EVENT");
+
+        // request parameters
+        String name = null;
+        String description = null;
+        float price = -1;
+        boolean visibility = false;
+        String location = null;
+        int maxPartecipantsInternational = -1;
+        int maxPartecipantVolunteer = -1;
+        LocalDateTime eventStart = null;
+        LocalDateTime eventEnd = null;
+        LocalDateTime subscriptionStart = null;
+        LocalDateTime subscriptionEnd = null;
+        LocalDateTime withdrawalEnd = null;
+        int maxWaitingList = -1;
+        String attributes = null;
+        String thumbnail = null;
+        String poster = null;
+
+        // model
+        Event e = null;
+        Message m = null;
+
+        //set datetime format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-ddTHH:mm");
+
+        try {
+            // retrieves the request parameters
+            name = req.getParameter("name");
+            description = req.getParameter("description");
+            price = Float.parseFloat(req.getParameter("price"));
+            String visibility_temp = req.getParameter("visibility");
+            if (visibility_temp == "Visible"){
+                visibility=true;
+            }  
+            location = req.getParameter("location");
+            maxPartecipantsInternational = Integer.parseInt(req.getParameter("maxPartecipantsInternational"));
+            maxPartecipantVolunteer = Integer.parseInt(req.getParameter("maxPartecipantVolunteer"));
+            eventStart = LocalDateTime.parse(req.getParameter("eventStart"), formatter);
+            eventEnd = LocalDateTime.parse(req.getParameter("eventEnd"), formatter);
+            subscriptionStart = LocalDateTime.parse(req.getParameter("subscriptionStart"), formatter);
+            subscriptionEnd = LocalDateTime.parse(req.getParameter("subscriptionEnd"), formatter);
+            withdrawalEnd = LocalDateTime.parse(req.getParameter("withdrawalEnd"), formatter);
+            maxWaitingList = Integer.parseInt(req.getParameter("maxWaitingList"));
+            attributes = req.getParameter("attributes");
+            thumbnail = req.getParameter("thumbnail");
+            poster = req.getParameter("poster");
+
+            // set the name of the event as the resource in the log context
+            LogContext.setResource(req.getParameter("name"));
+
+            // creates a new employee from the request parameters
+            e = new Event(-1L, name, description, price, visibility, location, maxPartecipantsInternational, 
+                maxPartecipantVolunteer, java.sql.Date.valueOf(eventStart.toLocalDate()), 
+                java.sql.Date.valueOf(eventEnd.toLocalDate()), java.sql.Date.valueOf(subscriptionStart.toLocalDate()),
+                java.sql.Date.valueOf(subscriptionEnd.toLocalDate()), java.sql.Date.valueOf(withdrawalEnd.toLocalDate()), 
+                 maxWaitingList, attributes, thumbnail, poster);
+
+            // creates a new object for accessing the database and stores the event
+            new AdminCreateEventDAO(getConnection(), e).access();
+
+            m = new Message(String.format("Event %d successfully created.", name));
+
+            LOGGER.info("Event %d successfully created in the database.", name);
+
+        } catch (NumberFormatException ex) {
+            m = new Message(
+                    "Cannot create the employee. Invalid input parameters.",
+                    "E100", ex.getMessage());
+
+            LOGGER.error(
+                    "Cannot create the employee. Invalid input parameters.",
+                    ex);
+        } catch (SQLException ex) {
+            
+            m = new Message("Cannot create the event: unexpected error while accessing the database.", "E200",
+                    ex.getMessage());
+
+            LOGGER.error("Cannot create the event: unexpected error while accessing the database.", ex);
+            
+        }
+
+    }
+
+}
