f39290abdf0a8d42f4e116249d82ddbd1c63618d
md.imran.faruck <mdimranfaruck.talukder@studenti.unipd.it>
2023-04-17 17:34:26 +0200

    User ass. Form Dao fixed

--- proj/src/main/java/com/essentls/dao/UserAssociationFormSubmitDAO.java
+++ proj/src/main/java/com/essentls/dao/UserAssociationFormSubmitDAO.java
@@ -47,7 +47,7 @@ public class UserAssociationFormSubmitDAO extends AbstractDAO<User> {
 
         try {
             stmt = con.prepareStatement(STATEMENT);
-            stmt.setInt(1, user.getCardId());
+            stmt.setString(1, user.getCardId());
             stmt.setString(2, user.getName());
             stmt.setString(3, user.getSurname());
             stmt.setString(4, user.getSex());
@@ -56,7 +56,7 @@ public class UserAssociationFormSubmitDAO extends AbstractDAO<User> {
             stmt.setString(7, user.getHomeCountryAddress());
             stmt.setString(8, user.getHomeCountryUniversity());
             stmt.setString(9, user.getPeriodOfStay());
-            stmt.setInt(10, user.getPhoneNumber());
+            stmt.setString(10, user.getPhoneNumber());
             stmt.setString(11, user.getPaduaAddress());
             stmt.setString(12, user.getDocumentType());
             stmt.setString(13, user.getDocumentNumber());
