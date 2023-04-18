a2a29e283374249067a7e5319505321f653e531e
md.imran.faruck <mdimranfaruck.talukder@studenti.unipd.it>
2023-04-19 01:04:43 +0200

    Database updated as recommanded & In every tables 5 rows of data added

--- proj/src/main/db/Table Creation Command.sql
+++ proj/src/main/db/Table Creation Command.sql
@@ -1,39 +1,41 @@
 
 -- Users Table Create Command  
 
+
 CREATE TYPE gen AS ENUM ('female', 'male', 'others');
+CREATE TYPE identity AS ENUM ('ID', 'Passport', 'Driver license');
+CREATE TYPE diet AS ENUM ('No specific', 'Vegetarian', 'Vegan', 'Halal', 'Kosher', 'Pescatarian');
 
 CREATE TABLE public."Users" (
  
 id SERIAL PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
 password VARCHAR(255) NOT NULL,
-cardID VARCHAR(50),
+"cardID" VARCHAR(50),
 tier INT DEFAULT 0,
-registrationDate DATE,
+"registrationDate" DATE,
 name VARCHAR(50),
 surname VARCHAR(50),
 sex gen,
-dateOfBirth DATE, 
+"dateOfBirth" DATE, 
 nationality VARCHAR(100),
-homeCountryAddress json,
-homeCountryUniversity VARCHAR(150),  
-periodOfStay VARCHAR(50),
-phoneNumber VARCHAR(50),
-paduaAddress json, 
-documentType VARCHAR(50),
-documentNumber VARCHAR(50),
-documentFile text,
-dietType text ARRAY,
+"homeCountryAddress" json,
+"homeCountryUniversity" VARCHAR(150),  
+"periodOfStay" INT,
+"phoneNumber" VARCHAR(50),
+"paduaAddress" json, 
+"documentType" identity,
+"documentNumber" VARCHAR(50),
+"documentFile" text,
+"dietType" diet,
 allergies text ARRAY,
-emailHash text,
-emailConfirmed Boolean DEFAULT FALSE
+"emailHash" VARCHAR(255),
+"emailConfirmed" Boolean DEFAULT FALSE
 );
 
 
 -- Events Table Create Command 
 
--- String imageUrl = "file:///C:/myimages/image.jpg"
 
 CREATE TABLE public."Events" (
 
@@ -43,14 +45,14 @@ description text,
 price numeric(10,2) CHECK (price >= 0), 
 visibility INT DEFAULT 3,
 location json,
-maxParticipantsInternational INT,
-maxParticipantsVolunteer INT,
-eventStart timestamp without time zone, 
-eventEnd timestamp without time zone, 
-subscriptionStart timestamp without time zone, 
-subscriptionEnd timestamp without time zone, 
-withdrawalEnd timestamp without time zone, 
-maxWaitingList INT,
+"maxParticipantsInternational" INT,
+"maxParticipantsVolunteer" INT,
+"eventStart" timestamp without time zone, 
+"eventEnd" timestamp without time zone, 
+"subscriptionStart" timestamp without time zone, 
+"subscriptionEnd" timestamp without time zone, 
+"withdrawalEnd" timestamp without time zone, 
+"maxWaitingList" INT,
 attributes text ARRAY,
 thumbnail text,
 poster text
@@ -59,24 +61,25 @@ poster text
 
 -- Payments Table Create Command 
 
+CREATE TYPE payMethod AS ENUM ('Cash', 'Card', 'Bank');
 
 CREATE TABLE public."Payments" (
 
 id SERIAL PRIMARY KEY,
-userId INT NOT NULL,  
-eventId INT,
-method text NOT NULL,
+"userId" INT NOT NULL,  
+"eventId" INT,
+method payMethod,
 amount numeric(10,2) CHECK (amount >= 0) NOT NULL,
 date DATE NOT NULL DEFAULT CURRENT_DATE,
 notes text, 
 
 CONSTRAINT fk_user 
-	  FOREIGN KEY(userId) 
+	  FOREIGN KEY("userId") 
 	  REFERENCES public."Users"(id)
 	  ON DELETE SET NULL, 
 
 CONSTRAINT fk_Event
-      FOREIGN KEY(eventId) 
+      FOREIGN KEY("eventId") 
 	  REFERENCES public."Events"(id)
 	  ON DELETE SET NULL
 
@@ -89,7 +92,7 @@ CONSTRAINT fk_Event
 CREATE TABLE public."Causes" (
 
 id SERIAL PRIMARY KEY,
-name text NOT NULL
+name VARCHAR(255)
 
 );
 
@@ -99,7 +102,7 @@ name text NOT NULL
 
 CREATE TABLE public."Tags" (
 
-name text,
+name VARCHAR(255),
 PRIMARY KEY (name)
 
 
@@ -107,27 +110,29 @@ PRIMARY KEY (name)
 
 -- Participants Table Create Command 
 
+CREATE TYPE roleTypes AS ENUM ('Organizer', 'Participant', 'Volunteer', 'WaitingList');
 
 
 CREATE TABLE public."Participants" (
-userId INT,
-eventId INT, 
-role text,
+"userId" INT,
+"eventId" INT, 
+role roleTypes,
 date timestamp without time zone,
-attributeValues json,
+"attributeValues" json,
 
-PRIMARY KEY (userId, eventId),
+PRIMARY KEY ("userId", "eventId"),
 CONSTRAINT fk_user 
-	  FOREIGN KEY(userId) 
+	  FOREIGN KEY("userId") 
 	  REFERENCES public."Users"(id)
 	  ON DELETE SET NULL, 
 
 CONSTRAINT fk_Event
-      FOREIGN KEY(eventId) 
+      FOREIGN KEY("eventId") 
 	  REFERENCES public."Events"(id)
 	  ON DELETE SET NULL
 
 
 );
+
 
 
