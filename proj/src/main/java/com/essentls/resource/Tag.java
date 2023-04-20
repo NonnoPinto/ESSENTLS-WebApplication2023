ea5a3136499a6349240ef76ca3b30c20aa0068a3
Vittorio Cardillo <vitto.cardillo.21@gmail.com>
2023-04-20 02:42:46 +0200

    Added tag resource

---
+++ proj/src/main/java/com/essentls/resource/Tag.java
@@ -0,0 +1,18 @@
+package com.essentls.resource;
+
+public class Tag {
+    private String name;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag [name=" + name + "]";
+    }
+}
