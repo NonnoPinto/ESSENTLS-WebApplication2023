eacead77335631455f477a12267403cf3575622d
Francesco Marcato <49764729+marcatofrancesco@users.noreply.github.com>
2023-05-22 21:27:50 +0200

    Edit and Create event layout fix + causesIDfromDAO

---
+++ proj/src/main/java/com/essentls/dao/CausesFromEventDAO.java
@@ -0,0 +1,53 @@
+package com.essentls.dao;
+
+import com.essentls.resource.Cause;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class CausesFromEventDAO extends AbstractDAO<ArrayList<Cause>> {
+
+    private static final String STATEMENT_CAUSE_LIST = "SELECT * FROM \"EventCauses\" LEFT JOIN \"Causes\" ON \"EventCauses\".\"causeId\" = \"Causes\".id WHERE \"EventCauses\".\"eventId\" = ?";
+
+    private int eventID = -1;
+
+    public CausesFromEventDAO(Connection con, int eventId) {
+        super(con);
+        this.eventID = eventId;
+    }
+
+    @Override
+    protected void doAccess() throws Exception {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        //the results of the search by a given eventId
+        final ArrayList<Cause> causes = new ArrayList<Cause>();
+
+        try {
+            pstmt = con.prepareStatement(STATEMENT_CAUSE_LIST);
+            pstmt.setInt(1, eventID);
+
+            rs = pstmt.executeQuery();
+
+            while (rs.next()){
+                causes.add(new Cause(rs.getInt("id"),rs.getString("name")));
+            }
+
+            LOGGER.info("%d Cause(s) successfully listed for eventId: %d.", causes.size(), eventID);
+
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        }
+
+        this.outputParam = causes;
+
+    }
+}
