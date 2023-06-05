39c4bb464d8649760a694c51e0279e6b720b6e0b
Giovanni Zago <giovanni.zago.3@studenti.unipt.it>
2023-06-05 19:16:07 +0200

    todo eliminated

--- proj/src/main/java/com/essentls/dao/AdminCreateUserDAO.java
+++ proj/src/main/java/com/essentls/dao/AdminCreateUserDAO.java
@@ -15,7 +15,7 @@ import org.postgresql.util.PGobject;
 
 public class AdminCreateUserDAO extends AbstractDAO<User>{
 
-    //TODO: need to create servlet and jsp page for admin user creation functionality and update statement with new database format
+    // This DAO is not used in the current version of the project since we figured out that the user creation by admin is currently not necessary
 
     /**
 	 * The SQL statement to be executed
